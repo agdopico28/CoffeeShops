3d32d674d0b7cf3cb7ea9198598b98226806ec5b
alu10701951 <alugar1951@ieselcaminas.org>
2023-11-22 17:13:54 +0100

    movimientos en los comentarios, rotacion sobre el eje x

--- app/src/main/java/com/example/coffeeshops/Comentarios.kt
+++ app/src/main/java/com/example/coffeeshops/Comentarios.kt
@@ -2,6 +2,7 @@ package com.example.coffeeshops
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -44,6 +45,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
@@ -139,11 +142,14 @@ fun getComentario(): List<String> {
 @Composable
 fun ItemsComentario(comentario: Int) { //crea las cartas que llevan los comentarios
     val coment = getComentario()
+    var rotationState by remember { mutableStateOf(0f) } //la posicion inicial a la que esta el comeentario
+    val rotation by animateFloatAsState(targetValue = rotationState) //hace que este animada la rotacion
     Card(
         Modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .clickable {},
+            .clickable { rotationState += 180f }//hace que gire 180 grados
+            .graphicsLayer(rotationX = rotation),//si esto lo cambias por la variable rotationState hara la rotacion pero a golpes
         elevation = CardDefaults.cardElevation(10.dp)
     ) {
         Column(
